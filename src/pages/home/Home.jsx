011cd162f5d33a46801cbf79d03856d953d3f787
ahad <akberqureshi361@gmail.com>
2024-01-06 19:06:00 +0530

    Create Navbar with responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,16 +3,13 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext'
 
 function Home() {
-  const context = useContext(myContext);
-
-  const { state,color} = context
+  
  
 
   return (
     <Layout>
-      <h1>name : {state.name}</h1>
-      <h1>Roll no : {state.rollno}</h1>
-      <h1>Roll no : {color}</h1>
+    Home
+    
     </Layout>
   )
 }
